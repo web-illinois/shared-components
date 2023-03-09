f49627633b2f5f456f5f96b519953d891f071c52
Bryan Jonker <jonker@illinois.edu>
2023-03-09 12:50:05 -0600

    Add links, update readme to be in production

--- ils-status/README.md
+++ ils-status/README.md
@@ -8,6 +8,8 @@ jonker@illinois.edu
 
 Development on 3/1/2023
 
+Production on 3/9/2023
+
 ## Dependencies and Examples
 
 Requires Illinois Toolkit, any version. Everything else is vanilla javascript/CSS.
@@ -16,18 +18,24 @@ This will add a component that queries a hosted Illinois API to get information 
 
 [Example of use](https://contrib.webtheme.illinois.edu/ils-status/example.html)
 
-Required code to use
+Required code to use in the header:
 
     <script src="//contrib.webtheme.illinois.edu/ils-status/status.min.js"></script>
     <link rel="stylesheet" href="//contrib.webtheme.illinois.edu/ils-status/status.min.css" media="all">
 
+Required code to use in the body:
+
+    <div class="ils-status"></div>
+
 The passthrough query is at https://statushub.itpartners.illinois.edu/api/GetSimpleStatus?name=illinois 
 
-The github is at https://github.com/itpartnersillinois/StatusSupport.
+The github for this middleware project is at https://github.com/itpartnersillinois/StatusSupport.
 
 ## Possible expansions
 
-Right now, this is just an simple passthrough of the public status with a one minute cache. We can add additional components in this to alter the JSON feed and display it appropriately. 
+Right now, this is just an simple passthrough of the status with caching enabled. We can add additional components in this to alter the JSON feed and display it appropriately. 
+
+This is hardcoded to use the Illinois status hub. We can change it to check other status hub information. 
 
 ## Userlist
 
--- ils-status/status.js
+++ ils-status/status.js
@@ -11,18 +11,19 @@ function checkStatus() {
     fetch('https://statushub.itpartners.illinois.edu/api/GetSimpleStatus?name=illinois', { method: 'GET', headers: { "Content-type": "application/x-www-form-urlencoded; charset=UTF-8" } })
         .then(res => res.json())
         .then(data => {
-            target.innerHTML = `<div class='row'><div class='bubble'><div class='green'>${data.NumberUp}</div><div>up</div></div>
-            <div class='bubble'><div class='yellow'>${data.NumberAffected}</div><div>affected</div></div>
-            <div class='bubble'><div class='red'>${data.NumberDown}</div><div>down</div></div><div>`;
+            target.innerHTML = `<a id='ils-status-link' href='https://illinois.statushub.io' aria-label='Go to the Status at Illinois page'><span class='ils-status-row'>
+            <span class='ils-status-bubble'><span class='ils-status-green'>${data.NumberUp}</span><span>up</span></span>
+            <span class='ils-status-bubble'><span class='ils-status-yellow'>${data.NumberAffected}</span><span>affected</span></span>
+            <span class='ils-status-bubble'><span class='ils-status-red'>${data.NumberDown}</span><span>down</span></span><span></a>`;
             if (data.DownServices.length > 0) {
-                target.innerHTML += '<p>The following services are down</p><ul>';
+                target.innerHTML += '<p>The following services are down: </p><ul>';
                 data.DownServices.forEach(d => {
                     target.innerHTML += `<li><a href=${d.Url}>${d.Name}</a><br>${d.Incident}</li>`
                 });
                 target.innerHTML += '</ul>'
             }
             if (data.AffectedServices.length > 0) {
-                target.innerHTML += '<p>The following services are affected</p><ul>';
+                target.innerHTML += '<p>The following services are affected: </p><ul>';
                 data.AffectedServices.forEach(a => {
                     target.innerHTML += `<li><a href=${a.Url}>${a.Name}</a><br>${a.Incident}</li>`
                 });
