59d3bddb49d1484d0a38fcb6fdce6724d439ed36
Bryan Jonker <jonker@illinois.edu>
2025-03-17 10:13:14 -0500

    Minor updates

--- ils-input/input3.js
+++ ils-input/input3.js
@@ -6,29 +6,31 @@ document.addEventListener('DOMContentLoaded', function (event) {
         }
     });
     document.querySelectorAll('button[aria-pressed]').forEach(item => {
-        let textSpan = document.createElement('span');
-        let iconSpan = document.createElement('ilw-icon');
-        iconSpan.setAttribute('size', '18px');
-        iconSpan.setAttribute('style', 'padding-left: 5px; vertical-align: middle;');
-        if (item.getAttribute('aria-pressed') == "true") {
-            textSpan.innerText = item.getAttribute('data-ils-input-on');
-            iconSpan.setAttribute('icon', 'more');
-        } else {
-            textSpan.innerText = item.getAttribute('data-ils-input-off');
-            iconSpan.setAttribute('icon', 'cancel');
-        }
-        item.append(textSpan);
-        item.append(iconSpan);
-        item.addEventListener('click', function(event) {
-            if (this.getAttribute('aria-pressed') == "true") {
-                this.setAttribute('aria-pressed', "false");
-                this.children[0].innerText = item.getAttribute('data-ils-input-off');
-                this.children[1].setAttribute('icon', 'cancel');
+        if (item.childNodes.length == 0) {
+            let textSpan = document.createElement('span');
+            let iconSpan = document.createElement('ilw-icon');
+            iconSpan.setAttribute('size', '18px');
+            iconSpan.setAttribute('style', 'padding-left: 5px; vertical-align: middle;');
+            if (item.getAttribute('aria-pressed') == "true") {
+                textSpan.innerText = item.getAttribute('data-ils-input-on');
+                iconSpan.setAttribute('icon', 'more');
             } else {
-                this.setAttribute('aria-pressed', "true");
-                this.children[0].innerText = item.getAttribute('data-ils-input-on');
-                this.children[1].setAttribute('icon', 'more');
+                textSpan.innerText = item.getAttribute('data-ils-input-off');
+                iconSpan.setAttribute('icon', 'cancel');
             }
-        })
+            item.append(textSpan);
+            item.append(iconSpan);
+            item.addEventListener('click', function(event) {
+                if (this.getAttribute('aria-pressed') == "true") {
+                    this.setAttribute('aria-pressed', "false");
+                    this.children[0].innerText = item.getAttribute('data-ils-input-off');
+                    this.children[1].setAttribute('icon', 'cancel');
+                } else {
+                    this.setAttribute('aria-pressed', "true");
+                    this.children[0].innerText = item.getAttribute('data-ils-input-on');
+                    this.children[1].setAttribute('icon', 'more');
+                }
+            })
+        }
     });
 });
